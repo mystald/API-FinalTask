b4442be94906fd9b6678623824fd71a816a79a88
Mystald <hilmanabdan28@gmail.com>
2020-07-26 14:33:07 +0700

    Added EmotionDB variable

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from model import *
 
 app = Flask(__name__)
 vect, model = loadModel()
+emotionDB = {}
 
 @app.route('/predict', methods=['POST'])
 def doPredict():    
     text = [request.values.get('text', type = str)]
-    print(text)
+    name = [request.values.get('name', type=str)]
+    print(nama, text)
     result = doPrediction(text, vect, model)
+    
+    try:
+        emotionDB[name].append(result[0])
+    except:
+        emotionDB[name] = [result[0]]
 
+    print(emotionDB[name])
     chat = {'chats': [
         {
             'text': 'Emosi anda saat ini : ' + result[0],
